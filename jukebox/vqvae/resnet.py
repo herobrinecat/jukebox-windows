e3458f61d43099a4f3a46d3f548c28208d0faa81
NOAAWeatherRadioS <89881391+NOAAWeatherRadioS@users.noreply.github.com>
2024-01-03 19:37:25 -0600

    Update resnet.py

--- jukebox/vqvae/resnet.py
+++ jukebox/vqvae/resnet.py
@@ -60,7 +60,7 @@ class Resnet1D(nn.Module):
             blocks = blocks[::-1]
         self.checkpoint_res = checkpoint_res
         if self.checkpoint_res == 1:
-            if dist.get_rank() == 0:
+            if 0 == 0:
                 print("Checkpointing convs")
             self.blocks = nn.ModuleList(blocks)
         else:
